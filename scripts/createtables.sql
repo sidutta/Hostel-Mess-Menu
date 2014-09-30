e82494db7e50a566fae1b489e39d5b569d9c93b6
Siddhartha <siddhartha_dutta@live.com>
2014-09-30 18:11:32 +0530

    added sql script to create table/ types

---
+++ scripts/createtables.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS foodtype;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS servings;
+DROP TABLE IF EXISTS fooditems;
+DROP TABLE IF EXISTS users;
+
+DROP TYPE IF EXISTS itemtype;
+DROP TYPE IF EXISTS hostel;
+
+CREATE TYPE itemtype AS ENUM ('Breakfast','Lunch','Tiffin','Dinner','Midnight');
+CREATE TYPE hostel AS ENUM ('H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12','H13','H14','H15');
+
+CREATE TABLE IF NOT EXISTS fooditems (itemid numeric PRIMARY KEY, itemname varchar NOT NULL);
+CREATE TABLE IF NOT EXISTS users (userid numeric PRIMARY KEY, username varchar NOT NULL, hostelnumber hostel NOT NULL);
+CREATE TABLE IF NOT EXISTS foodtype (itemid numeric REFERENCES fooditems(itemid) ON DELETE RESTRICT, type itemtype NOT NULL);
+CREATE TABLE IF NOT EXISTS servings (sid numeric PRIMARY KEY, itemid numeric REFERENCES fooditems(itemid) ON DELETE RESTRICT NOT NULL, type itemtype NOT NULL, servedon date NOT NULL, hostelnumber hostel NOT NULL);
+CREATE TABLE IF NOT EXISTS reviews (userid numeric REFERENCES users(userid), sid numeric REFERENCES servings(sid) ON DELETE RESTRICT, rating numeric CHECK(rating>=0 AND rating<=5) NOT NULL, review varchar(150));
